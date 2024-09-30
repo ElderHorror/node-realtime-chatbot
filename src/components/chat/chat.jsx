8620ee7dc41ec2da4a8cb700107e292e69015acd
Adedeji Adebayo <adebayoadedeji110@gmail.com>
2024-09-30 20:04:08 +0100

    Created Chat Function

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,8 +1,104 @@
-import './chat.css'
-const Chat = () => {
-  return (
-    <div className='chat'>Chat</div>
-  )
-}
+import { useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
-export default Chat
+const Chat = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleClick = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+
+  console.log(text);
+
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>John Doe</span>
+            <p>Lorem ipsum dolor</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <span>1 hour ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <img src="" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <span>1 hour ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <span>1 hour ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <img src="" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="message">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+          </div>
+          <div className="message own">
+            <img src="" alt="" />
+            <div className="texts">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <span>1 hour ago</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png " alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleClick} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
--- src/components/list/chatlist/ChatList.jsx
+++ src/components/list/chatlist/ChatList.jsx
@@ -4,7 +4,6 @@ const ChatList = () => {
   const [addMode, setAddMode] = useState(false);
   return (
     <div className="chatList">
-      ChatList
       <div className="search">
         <div className="searchBar">
           <img src="/search.png" alt="" />
